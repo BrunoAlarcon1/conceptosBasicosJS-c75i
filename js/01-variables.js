5a024e415063166b43c0721103c5078e1c6dad04
bruno.alarcon <alarconbruno14@gmail.com>
2023-10-26 22:27:39 -0300

    Agregamos un mensaje con document.write y un alert

--- js/01-variables.js
+++ js/01-variables.js
@@ -1,6 +1,17 @@
 let anioActual = 2023;
+const universe='https://universe.rollingcodeschool.com/';
 
 /* Mostrar mensaje */
 console.log(anioActual);
 console.log("anioActual");
 console.log("Año Actual: " + anioActual)
+console.log(universe);
+
+/* Modificar el contenido de una variable */
+anioActual = 2024;
+console.log(anioActual);
+
+document.write('<p class="parrafoDestacado">Esto es una prueba de document.write</p>');
+
+/* Ventanas emergentes */
+alert('Hola Mundo');
