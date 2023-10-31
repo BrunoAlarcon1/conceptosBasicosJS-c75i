51cb599569bd12805d5e1a3eaa20ff976f461154
bruno.alarcon <alarconbruno14@gmail.com>
2023-10-30 22:42:39 -0300

    agregamos un else if

--- js/02-condicionales.js
+++ js/02-condicionales.js
@@ -7,15 +7,12 @@ if(condicion logica){
 /* Solicite la edad al usuario e indique si debe votar */
 /* 16  y menores de 18 es optativo, mayores de 70 */
 
-const edad = parseInt(prompt('Ingrese su edad'));
+const edad = parseInt(prompt("Ingrese su edad"));
 
-if(edad>=18 && edad<=70){
-    document.write('Debe votar');
-}else{
-    if((edad>=16 && edad<18) || edad>70){
-        document.write('Es optativo votar')
-    }else{
-    document.write('No debe votar');
-    }
+if (edad >= 18 && edad <= 70) {
+  document.write("Debe votar");
+} else if ((edad >= 16 && edad < 18) || edad > 70) {
+  document.write("Es optativo votar");
+} else {
+  document.write("No debe votar");
 }
- 
